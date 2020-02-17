0c8035325a96772ac3a481665e86be5c26aa93d3
PatrykWojcik <55151611+lacrossepw@users.noreply.github.com>
2020-02-17 08:55:01 +0100

    Add files via upload

---
+++ kolejnyphp.php
@@ -0,0 +1,90 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Untitled Document</title>
+</head>
+ 
+<body>
+<h3>Formularz zakupu samochodu</h3>
+ 
+
+<label>Imie&nbsp;&nbsp; <input type="text" name="varaible" /></label><br>
+    <label>Nazwisko <input type="text" name="lastName" /></label><br>
+    <label>Email&nbsp; <input type="text" name="email" /></label><br>
+    <br>
+    <select name="lista" id="">
+    <option value="option">Wybierz marke</option>
+    <option value="Ford">Ford Ranger</option>
+    <option value="Buick">Buick Riviera</option>
+    <option value="Pontiac">Pontiac Trans Am</option>
+    <option value="Dodge">Dodge Ram</option>
+    </select>
+    </br>
+    <br>
+    <select name="lista" id="">
+    <option value="option">Wybierz wersje</option>
+    <option value="Ford">Ford Ranger Raptor</option>
+    <option value="Ford">Ford Ranger V </option>
+    <option value="Ford">Ford Ranger IV </option>
+    <option value="Ford">Ford Ranger III </option>
+    <option value="Ford">Ford Ranger II</option>
+    <option value="Ford">Ford Ranger I</option>
+    <option value="Buick">Buick Riviera< VIII/option>
+    <option value="Buick">Buick Riviera VII</option>
+    <option value="Buick">Buick Riviera VI</option>
+    <option value="Buick">Buick Riviera V</option>
+    <option value="Buick">Buick Riviera IV</option>
+    <option value="Buick">Buick Riviera III</option>
+    <option value="Buick">Buick Riviera II</option>
+    <option value="Buick">Buick Riviera I</option>
+    <option value="Pontiac">Pontiac IV </option>
+    <option value="Pontiac">Pontiac III </option>
+    <option value="Pontiac">Pontiac II </option>
+    <option value="Pontiac">Pontiac I </option>
+    <option value="Dodge">Dodge Ram IV</option>
+    <option value="Dodge">Dodge Ram III</option>
+    <option value="Dodge">Dodge Ram II</option>
+    <option value="Dodge">Dodge Ram I</option>
+    </select>
+    </br>
+    <br>
+    <select name="lista" id="">
+    <option value="option">Wybierz silnik</option>
+    <option value="Ford">Ford 2.0 EcoBlue 213KM</option>
+    <option value="Ford">Ford 2.2 Duratorq 150KM</option>
+    <option value="Ford">Ford 3.2 Duratorq 200KM</option>
+    <option value="Pontiac">Pontiac 5.0 190KM</option>
+    <option value="Pontiac">Pontiac 5.7 V8 245KM</option>
+    <option value="Buick">Buick 7.5 V8 375KM</option>
+    <option value="Dodge">Dodge 3.7 V6 215KM</option>
+    <option value="Dodge">Dodge 5.7 V8 390KM</option>
+    <option value="Dodge">Dodge 4.7 V8 310KM</option>
+    </select>
+    </br>
+    <br>
+    Wybierz kolor </br>
+    <label>Czerwony<input type="checkbox" name="check1" id=""></label><br><br>
+    <label>Niebieski<input type="checkbox" name="check2" id=""></label><br><br>
+    <label>Pomarańcz<input type="checkbox" name="check3" id=""></label><br><br>
+    <label>Czarny<input type="checkbox" name="check4" id=""></label><br><br>
+    </br>
+    <br>
+    Wybierz wyposażenie </br>
+    <label>Nawigacja <input type="radio" name="nawigacja" id=""></label></br>
+    <label>Hak Holowniczy <input type="radio" name="hak" id=""></label></br>
+    <label>Czujniki <input type="radio" name="czujnik" id=""></label></br>
+    <label>Kamera cofania <input type="radio" name="kamera" id=""></label></br>
+    <label>Skórzane fotele <input type="radio" name="kamera" id=""></label></br>
+    <label>Aux <input type="radio" name="kamera" id=""></label></br>
+    <label>2 wejścia usb przy każdym fotelu <input type="radio" name="kamera" id=""></label></br>
+
+    </br>
+
+
+
+ 
+</br>
+
+</body>
+</html>
